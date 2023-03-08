a1ab0ec4133b5bf7d2a6c6c5e3afce5eb0f5cfc6
Ammar Qureshi <mianammarqureshi@gmail.com>
2023-03-08 15:16:44 +0000

    PlayMove changes

--- ChessGame/PlayMove.cs
+++ ChessGame/PlayMove.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ChessGame
 {
     class PlayMove
     {
-        public static void MakeMove(bool whiteToPlay) 
+        public static void MakeMove(bool whiteToPlay)
         {
             List<PLegal> move = MoveGeneration.move;
             char[,] boardArray = Board.ChessBoard;
@@ -16,17 +17,74 @@ namespace ChessGame
             int sFile = -1;
             int sRow = -1;
 
+            Console.WriteLine("Select piece to move: ");
+            sSquare = Console.ReadLine();
+            sFile = sSquare[0] - 97;
+            sRow = 8 - Convert.ToInt16(Char.GetNumericValue(sSquare[1]));
+            Console.WriteLine(sRow + " " + sFile);
+            int[] list = {6, 0};
+            int[] endList = { 5, 0 };
 
-            //need to make sure if white is playing you cannot move opponents piece.
-            while(sFile < 0 || sFile > 7 || sRow < 0 || sRow > 7 || boardArray[sRow, sFile] == '-' )
+            var result = from s in move
+                         where s.currentPos == list
+                         select s;
+            foreach(var results in result)
             {
-                Console.WriteLine("Select piece to move: ");
-                sSquare = Console.ReadLine();
-                sFile = sSquare[0] - 97;
-                sRow = 8 - Convert.ToInt16(Char.GetNumericValue(sSquare[1]));
-                Console.WriteLine(sFile + " " + sRow);
+                Console.WriteLine("ok");
             }
-            Console.WriteLine(boardArray[sRow, sFile]);
+            
         }
     }
 }
+
+
+/*
+            if (whiteToPlay)
+            {
+                while (sFile < 0 || sFile > 7 || sRow < 0 || sRow > 7 || boardArray[sRow, sFile] == '-' || friendlyPiece == true)
+                {
+                    Console.WriteLine("Select piece to move: ");
+                    sSquare = Console.ReadLine();
+                    sFile = sSquare[0] - 97;
+                    sRow = 8 - Convert.ToInt16(Char.GetNumericValue(sSquare[1]));
+                    Console.WriteLine(sRow + " " + sFile);
+                    //need to make sure if white is playing you cannot move opponents piece.
+                    foreach (char piece in blackPieces)
+                    {
+                        if (boardArray[sRow, sFile] == piece)
+                        {
+                            friendlyPiece = true;
+                            break;
+                        }
+                        else
+                        {
+                            friendlyPiece = false;
+                        }
+                    }
+                }
+            }
+            if (!whiteToPlay)
+            {
+                while (sFile < 0 || sFile > 7 || sRow < 0 || sRow > 7 || boardArray[sRow, sFile] == '-' || friendlyPiece == true)
+                {
+                    Console.WriteLine("Select piece to move: ");
+                    sSquare = Console.ReadLine();
+                    sFile = sSquare[0] - 97;
+                    sRow = 8 - Convert.ToInt16(Char.GetNumericValue(sSquare[1]));
+                    Console.WriteLine(sRow + " " + sFile);
+                    foreach (char piece in whitePieces)
+                    {
+                        if (boardArray[sRow, sFile] == piece)
+                        {
+                            friendlyPiece = true;
+                            break;
+                        }
+                        else
+                        {
+                            friendlyPiece = false;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(boardArray[sRow, sFile]);
+            */
